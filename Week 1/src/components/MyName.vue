1c59489e7dbfb94397650aa49dda6f135044bcf5
mjag0004 <mjag0004@student.monash.edu>
2025-08-19 23:10:08 +1000

    Initial commit

---
+++ Week 1/src/components/MyName.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script setup>
+const name = 'Monish Jagadish Hallegere'
+</script>
+
+<style lang="css" scoped>
+h1 {
+  color: #ffffff;
+}
+</style>
