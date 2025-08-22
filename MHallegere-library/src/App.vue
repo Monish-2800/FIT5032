98d98c1276aa46f1eb79df4c807cc89672cf72be
mjag0004 <mjag0004@student.monash.edu>
2025-08-22 22:39:40 +1000

    Created Form Vue Component

--- MHallegere-library/src/App.vue
+++ MHallegere-library/src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import JSON from './components/JSON.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+// import JSON from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
-      <JSON />
+      <Form />
+      <!-- <JSON /> -->
       <!-- <HelloWorld msg="You did it!" /> -->
     </div>
   </header>
